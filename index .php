142a604f55325568810a40d28670c84c0654c7df
jaab <jorgealfredoalvarezbenedetto@gmail.com>
2023-05-29 18:33:24 +0000

    version a subir

---
+++ index .php
@@ -0,0 +1,12 @@
+<FORM action='dos.php' method='post'>
+ equipo<input type='text' name='equipo'>
+       <input type='submit'>
+
+
+
+</FORM>
+
+
+	
+
+
---
+++ utils/saludar.php
@@ -0,0 +1,5 @@
+<?php
+    function saludar($m){
+       RETURN " VAMOS EL ".$m;
+    }
+?>
